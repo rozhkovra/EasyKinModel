d54ac1f80e539124f7de40d743ebcb6a97ea1962
lux_rozhkov <rrozhkov@luxoft.com>
2017-07-04 14:17:42 +0600

    EKIN-0026 Set deafult locale

---
+++ bin/jar.bat
@@ -0,0 +1,5 @@
+﻿REM
+cd ..\target
+copy  easykin.model-5.0.1-SNAPSHOT.jar ..\..\easykin\lib
+copy  easykin.model-5.0.1-SNAPSHOT.jar ..\..\easykin.android\app\libs
+copy  easykin.model-5.0.1-SNAPSHOT.jar ..\..\easykin.web\src\main\webapp\WEB-INF\lib
